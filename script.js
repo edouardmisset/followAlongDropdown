63ebb2326cdaa6fe70c35d20e2efdb92d78091bb
Edouard Misset <edouardmisset@gmail.com>
2021-02-17 19:13:27 +0100

    follow along

---
+++ script.js
@@ -0,0 +1,43 @@
+// Grabbing the list items of the nav bar & dropdown
+const navItems = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+// Adding Event listeners
+navItems.forEach((element) =>
+  element.addEventListener("mouseenter", handleEnter)
+);
+navItems.forEach((element) =>
+  element.addEventListener("mouseleave", handleLeave)
+);
+
+// The main function to display the dropdown
+function handleEnter() {
+  this.classList.add("trigger-enter");
+  setTimeout(() => {
+    if (this.classList.contains("trigger-enter")) {
+      this.classList.add("trigger-enter-active");
+    }
+  }, 150);
+  background.classList.add("open");
+
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCoordinates = dropdown.getBoundingClientRect();
+  const navCoordinates = nav.getBoundingClientRect();
+
+  const { width, height } = dropdownCoordinates;
+
+  const top = dropdownCoordinates.top - navCoordinates.top;
+  const left = dropdownCoordinates.left - navCoordinates.left;
+
+  background.style.setProperty("width", `${width}px`);
+  background.style.setProperty("height", `${height}px`);
+
+  background.style.setProperty("transform", `translate(${left}px, ${top}px)`);
+}
+
+// Function to handle the leave event
+function handleLeave() {
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
+}
